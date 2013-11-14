eb45e8bb114f33a7307350fee74f5e12baf642e0
ilkeryasinyildiz <ilkeryasinyildiz@gmail.com>
2013-11-14 12:32:01 +0000

    SHA256Hasher ve LoggingAspect sınıfları oluşturuldu ve gerekli yerlerle bağlantısı kuruldu

--- src/org/sourcelesslight/hashing/SHA256Hasher.java
+++ src/org/sourcelesslight/hashing/SHA256Hasher.java
@@ -1,5 +1,38 @@
 package org.sourcelesslight.hashing;
 
+import java.security.MessageDigest;
+
 public class SHA256Hasher {
 
+	/* Encrypt the string with SHA-256 and return the hashcode */
+	public String encrypt(String password)
+	{ 
+        try{
+				MessageDigest md = MessageDigest.getInstance("SHA-256");
+		        md.update(password.getBytes());
+		 
+		        byte byteData[] = md.digest();
+		 
+		        //convert the byte to hex format method 1
+		        StringBuffer sb = new StringBuffer();
+		        for (int i = 0; i < byteData.length; i++) {
+		         sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+		        }
+		 
+		        //convert the byte to hex format method 2
+		        StringBuffer hexString = new StringBuffer();
+		    	for (int i=0;i<byteData.length;i++) {
+		    		String hex=Integer.toHexString(0xff & byteData[i]);
+		   	     	if(hex.length()==1) hexString.append('0');
+		   	     	hexString.append(hex);
+		    	}
+		    	return hexString.toString();
+        }
+        catch(Exception e)
+        {
+        	e.printStackTrace();
+        	return null;
+        }
+	}
+	
 }
