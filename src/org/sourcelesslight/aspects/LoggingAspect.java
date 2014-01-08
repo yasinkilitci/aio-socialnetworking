4a60e0b7698fa37385927b1248149aab9ee4b5a4
yasinkilitci <yasinkilitci@gmail.com>
2014-01-08 01:15:20 +0000

    get local ip minor aop changes logout aspect but not working

--- src/org/sourcelesslight/aspects/LoggingAspect.java
+++ src/org/sourcelesslight/aspects/LoggingAspect.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.sourcelesslight.model.User;
 import org.springframework.context.MessageSource;
@@ -20,23 +21,35 @@ public class LoggingAspect {
 	 * org.sourcelesslight.services.AuthenticationService.performLogin(any parameter)
 	 * Joinpoint: helps us catch the parameters of the method specified
 	 */
+//	@AfterReturning(value = "@target(org.sourcelesslight.services.AuthenticationService) && !execution(* performLogin*(..))", returning = "returnValue")
 	@After("execution(public * org.sourcelesslight.services.AuthenticationService.performLogin(..))")
 	public void logging_LoginAdvice(JoinPoint joinpoint){
-		Object args[] = joinpoint.getArgs();
-		String username = (String)args[0];
-		String password = (String)args[1];
-		//String ip = (String)args[2];
-		logger.warn(messageSource.getMessage("L001",new Object[]{username,password},Locale.US));
-	}
+				Object args[] = joinpoint.getArgs();
+		 		String username = (String)args[0];
+		 		String password = (String)args[1];
+		 		String ip = (String)args[2];
+		 		logger.warn(messageSource.getMessage("L001",new Object[]{username,password,ip},Locale.US));
+			}
 	
 	@After("execution(public * org.sourcelesslight.services.UserService.savePreferencesWithUser(..))")
-	public void Logging_SignupAdvice(JoinPoint joinpoint){
+	public void Logging_SignupAdvice(JoinPoint joinpoint ){
 		Object args[] = joinpoint.getArgs();
 		User user = (User)args[0];
 		//http://logging.apache.org/log4j/1.2/apidocs/org/apache/log4j/Level.html
 		// warn / fatal / trace 
 		logger.fatal(messageSource.getMessage("L002",new Object[]{user.getUsername(),user.getPassword()},Locale.US));
+
 	}
+	
+	@After("execution(public *  org.sourcelesslight.actions.LogoutAction.session.remove(..))")
+	public void Logging_LogoutAdvice(JoinPoint joinpoint){
+		
+		Object args[]=joinpoint.getArgs();
+		User user = (User)args[0];
+		logger.info(messageSource.getMessage("L003",new Object[]{user.getUsername(),user.getPassword()},Locale.US));
+
+	}
+	
 
 	public void setLogger(Logger logger) {
 		this.logger = logger;
